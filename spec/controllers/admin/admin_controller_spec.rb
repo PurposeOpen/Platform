9f5caa033dd6dd29ca0ed6c645c85a74652ca984
Diego Marcet <diego.marcet@tipit.net>
2013-04-09 09:30:57 -0300

    Revert "[diego] #2447 - Fixed ignored Admin::AdminController tests"
    This reverts commit 487c5244bedb1abb5fbe3f1017635acd0d001863.

--- spec/controllers/admin/admin_controller_spec.rb
+++ spec/controllers/admin/admin_controller_spec.rb
@@ -1,50 +1,36 @@
 require 'spec_helper'
 
 describe Admin::AdminController do
-  class Admin::AdminController
+  class DummyAdminController < Admin::AdminController;
     skip_authorize_resource
     skip_authorization_check
     def index; head :ok end
   end
 
-  def perform_request
-    with_routing do |map|
-      map.draw { match 'admin/(:movement_id)' => 'admin/admin#index' }
-      yield
-    end
-  end
-
   let(:walkfree) { FactoryGirl.create(:movement, :name => 'Walk Free') }
   before do
+    @controller = DummyAdminController.new
     login_as FactoryGirl.build(:admin_platform_user)
   end
 
-  it 'loads the movement based on the given movement ID' do
-    perform_request do
-      get :index, :movement_id => walkfree.id.to_s
-      assigns(:movement).should == walkfree
-    end
+  xit 'loads the movement based on the given movement ID' do
+    get :index, :movement_id => walkfree.id.to_s
+    assigns(:movement).should == walkfree
   end
 
-  it 'loads the movement based on the given movement slug' do
-    perform_request do
-      get :index, :movement_id => walkfree.slug
-      assigns(:movement).should == walkfree
-    end
+  xit 'loads the movement based on the given movement slug' do
+    get :index, :movement_id => walkfree.slug
+    assigns(:movement).should == walkfree
   end
 
-  it 'does not load any movement when neither ID or slug is provided' do
-    perform_request do
-      get :index
-      assigns(:movement).should be_nil
-    end
+  xit 'does not load any movement when neither ID or slug is provided' do
+    get :index
+    assigns(:movement).should be_nil
   end
 
-  it 'sets the default admin locale to English' do
-    perform_request do
-      I18n.locale = :pt
-      get :index
-      I18n.locale.should == :en
-    end
+  xit 'sets the default admin locale to English' do
+    I18n.locale = :pt
+    get :index
+    I18n.locale.should == :en
   end
 end
