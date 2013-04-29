f7de4927534242f6e0b2617804e8d7571dd24f3d
John McCarthy <post.john.mccarthy@gmail.com>
2013-04-29 12:17:54 -0400

    [NicoPaez] - Tests fixed

--- spec/controllers/api/health_dashboard_controller_spec.rb
+++ spec/controllers/api/health_dashboard_controller_spec.rb
@@ -1,28 +1,45 @@
 require 'spec_helper'
 
 describe Api::HealthDashboardController do
-  before :each do
-    Net::HTTP.stub(:get_response) { Net::HTTPOK.new(nil, 200, "") }
-    @movement = FactoryGirl.create(:movement)
+  describe "handling database failure" do
+
+    it "should return json CRITICAL if the database is down" do
+      ActiveRecord::Base.connection.should_receive(:execute).and_raise(ActiveRecord::StatementInvalid)
+      ActiveRecord::Base.connection.should_receive(:execute)
+      Delayed::Job.should_receive(:where).and_return([mock()])
+
+      get :index, :format => 'json', :movement_id=>1
+
+      services = JSON.parse(response.body)['services']
+      services['platform'].should eql "CRITICAL - database is down"
+      services['database'].should eql "CRITICAL - Error: ActiveRecord::StatementInvalid"
+    end
+
+    it "should return html CRITICAL if the database is down" do
+      ActiveRecord::Base.connection.should_receive(:execute).and_raise(ActiveRecord::StatementInvalid)
+      ActiveRecord::Base.connection.should_receive(:execute)
+      Delayed::Job.should_receive(:where).and_return([mock()])
+
+      get :index, :format => 'html', :movement_id=>1
+
+      response.should be_success
+      service_statuses = assigns(:service_statuses)
+      service_statuses[:services][:platform].should eql "CRITICAL - database is down"
+      service_statuses[:services][:database].should eql "CRITICAL - Error: ActiveRecord::StatementInvalid"
+    end
   end
 
   describe "responding to JSON" do
+    before :each do
+      Net::HTTP.stub(:get_response) { Net::HTTPOK.new(nil, 200, "") }
+      @movement = FactoryGirl.create(:movement)
+    end
+
     it "should return the overall status for the platform" do
       get :index, :format => 'json', :movement_id=>@movement.id
 
       services = JSON.parse(response.body)['services']
       services['platform'].should eql "OK"
-    end
-
-    it "should return CRITICAL if the database is down" do
-      ActiveRecord::Base.connection.should_receive(:execute).and_raise(ActiveRecord::StatementInvalid)
-      ActiveRecord::Base.connection.should_receive(:execute)
-
-      get :index, :format => 'json', :movement_id=>@movement.id
-
-      services = JSON.parse(response.body)['services']
-      services['platform'].should eql "CRITICAL - database is down"
-      services['database'].should eql "CRITICAL - Error: ActiveRecord::StatementInvalid"
     end
 
     it "should return OK if the database is up and able to handle queries" do
@@ -65,6 +82,11 @@ describe Api::HealthDashboardController do
   end
 
   describe "responding to HTML" do
+    before :each do
+      Net::HTTP.stub(:get_response) { Net::HTTPOK.new(nil, 200, "") }
+      @movement = FactoryGirl.create(:movement)
+    end
+
     it "should return the overall status for the platform" do
       get :index, :format => 'html', :movement_id=>@movement.id
 
@@ -72,18 +94,6 @@ describe Api::HealthDashboardController do
 
       service_statuses = assigns(:service_statuses)
       service_statuses[:services][:platform].should eql "OK"
-    end
-
-    it "should return CRITICAL if the database is down" do
-      ActiveRecord::Base.connection.should_receive(:execute).and_raise(ActiveRecord::StatementInvalid)
-      ActiveRecord::Base.connection.should_receive(:execute)
-
-      get :index, :format => 'html', :movement_id=>@movement.id
-
-      response.should be_success
-      service_statuses = assigns(:service_statuses)
-      service_statuses[:services][:platform].should eql "CRITICAL - database is down"
-      service_statuses[:services][:database].should eql "CRITICAL - Error: ActiveRecord::StatementInvalid"
     end
 
     it "should return OK if the database is up and able to handle queries" do
