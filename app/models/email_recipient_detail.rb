d8e1fc0483161c5d9fddb72d2c5914ffc5894c3b
NicoPaez <nicopaez@gmail.com>
2013-05-15 15:57:03 -0300

    [NicoPaez] - Modification in the way sent_to_users_ids is stored

--- app/models/email_recipient_detail.rb
+++ app/models/email_recipient_detail.rb
@@ -13,7 +13,7 @@ class EmailRecipientDetail < ActiveRecord::Base
     email_recipient_detail = self.new
     email_recipient_detail.email_id = email.id
     email_recipient_detail.recipients_count = recipients_id.size
-    email_recipient_detail.sent_to_users_ids = recipients_id
+    email_recipient_detail.sent_to_users_ids = recipients_id.join(',')
     email_recipient_detail
   end
 
--- spec/models/email_recipient_detail_spec.rb
+++ spec/models/email_recipient_detail_spec.rb
@@ -9,5 +9,12 @@ describe EmailRecipientDetail do
       email_recipient_detail = EmailRecipientDetail.create_with(email, [1,2,3])
       email_recipient_detail.recipients_count.should eq 3
     end
+
+    it 'should store recipients as comma separated string' do
+      email = FactoryGirl.create(:email, :test_sent_at => Time.now)
+      email_recipient_detail = EmailRecipientDetail.create_with(email, [1,2,3])
+      email_recipient_detail.sent_to_users_ids.should eq "1,2,3"
+    end
+
   end
 end
