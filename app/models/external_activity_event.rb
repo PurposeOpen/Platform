c461205a27fab12ae8c1f3016067afb4020a995e
John McCarthy <post.john.mccarthy@gmail.com>
2013-06-27 17:16:01 -0400

    [john] - annotated new field

--- app/models/external_activity_event.rb
+++ app/models/external_activity_event.rb
@@ -12,6 +12,7 @@
 #  user_id             :integer
 #  created_at          :datetime         not null
 #  updated_at          :datetime         not null
+#  activity            :string(255)
 #
 
 class ExternalActivityEvent < ActiveRecord::Base
