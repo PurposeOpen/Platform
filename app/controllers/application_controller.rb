2e4af80d0593b7814560cbc10420e367d66155cd
Scott Feinberg <feinberg.scott@gmail.com>
2013-07-19 18:54:42 -0400

    debug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,6 +23,7 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource_or_scope)
     case resource_or_scope
     when :platform_user, PlatformUser
+      Rails.logger.debug "Resource or Scope: #{resource_or_scope}"
       store_location = session[:return_to]
       clear_stored_location
       (store_location.nil?) ? '/' : store_location.to_s
