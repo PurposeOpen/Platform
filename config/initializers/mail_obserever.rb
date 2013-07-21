c237104b7d2c93ab3b6a75480438a58a1350f411
Scott Feinberg <feinberg.scott@gmail.com>
2013-07-20 20:17:15 -0400

    didn't remove the old file...

--- config/initializers/mail_obserever.rb
+++
@@ -1,7 +0,0 @@
-class MailObserver
-  def self.delivered_email(message)
-    Rails.logger.debug "LDEBUG: Actually Sent Message #{message}"
-  end
-end
-
-ActionMailer::Base.register_observer(MailObserver)
