153ac8c8e36d4ddbd69e2875d83bb4103e4a0ccc
Scott Feinberg <scott@allout.org>
2013-08-01 14:31:27 -0400

    index for content module links

---
+++ db/migrate/20130801173251_add_index_to_content_module_links.rb
@@ -0,0 +1,9 @@
+class AddIndexToContentModuleLinks < ActiveRecord::Migration
+  def up
+  	add_index :content_module_links, [:id,:page_id,:content_module_id,:position,:layout_container]
+  end
+
+  def down
+  	remove_index :content_module_links, [:id,:page_id,:content_module_id,:position,:layout_container]
+  end
+end
