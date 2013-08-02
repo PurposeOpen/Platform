5b67a5ac7b8ca2f5c4f18ead2bce4e80d30ecd7e
Scott Feinberg <scott@allout.org>
2013-08-02 10:14:33 -0400

    debug

--- app/controllers/api/action_pages_controller.rb
+++ app/controllers/api/action_pages_controller.rb
@@ -128,7 +128,8 @@ class Api::ActionPagesController < Api::BaseController
 
   def action_info_from(params)
     action_info = (params[:action_info].is_a? Hash) ? params[:action_info] : {}
-    action_info.merge(:email => identify_email)
+    member_info = (params[:member_info].is_a? Hash) ? params[:member_info] : {}
+    action_info.merge(:email => member_info[:email])
   end
 
 end
