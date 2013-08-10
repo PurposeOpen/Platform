fbf184d21e234b8b0ac725ae864e65a974fdbb3d
Scott Feinberg <scott@allout.org>
2013-08-10 16:58:42 -0400

    look at identify_lang instead of locale

--- app/controllers/api/movements_controller.rb
+++ app/controllers/api/movements_controller.rb
@@ -7,11 +7,11 @@ class Api::MovementsController < Api::BaseController
 
   def show
     homepage = params[:draft_homepage_id].blank? ? movement.homepage : movement.draft_homepages.where(:id => params[:draft_homepage_id]).first
-    homepage_key = 'homepage_content/movement/#{movement.id}/locale/#{I18n.locale}/homepage/#{homepage.id}'
+    homepage_key = 'homepage_content/movement/#{movement.id}/locale/#{identify_accepted_language}/homepage/#{homepage.id}'
     @homepage_content = Rails.cache.fetch(homepage_key, expires_in: 24.hours) do
       build_homepage_content(homepage) 
     end
-    featured_content_collections_key = 'featured_content_collections/movement/#{movement.id}/locale/#{I18n.locale}/homepage/#{homepage.id}'
+    featured_content_collections_key = 'featured_content_collections/movement/#{movement.id}/locale/#{identify_accepted_language}/homepage/#{homepage.id}'
     @featured_content_collections = Rails.cache.fetch(featured_content_collections_key, expires_in: 24.hours) do
       build_featured_content_collections(homepage)
     end
